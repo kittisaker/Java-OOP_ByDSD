dc3cc90c94bd902a834effcbc68b23f03c58cca5
kittisaker <kittisak.hanheam@gmail.com>
2023-09-23 14:28:01 +0700

    update chapter-11

---
+++ th/go/dsd/util/Mypack.java
@@ -0,0 +1,10 @@
+package th.go.dsd.util;
+
+public class Mypack {
+
+    public static void main(String[] a) {
+        for(String s : a){
+            System.out.println("Value = " + s);
+        }
+    }
+}
